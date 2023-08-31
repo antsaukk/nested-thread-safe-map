077a736467c78170964f4826eb758609ac0017d4
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2023-08-31 14:10:11 +0300

    update test async

--- cmap_one2one/main.cpp
+++ cmap_one2one/main.cpp
@@ -21,8 +21,9 @@ void TestSimple()
 
 void RunConcurrentUpdates(
     cmap_fold& cm, size_t thread_count, int key_count
-) {
-  auto kernel = [&cm, key_count](int seed)
+)
+{
+  auto kernel_inner = [&cm, key_count](int seed)
   {
     vector<int> updates(key_count);
     iota(begin(updates), end(updates), -key_count / 2);
@@ -45,17 +46,39 @@ void RunConcurrentUpdates(
     
   };
 
+  auto kernel_outer = [&cm, thread_count](int seed)
+  {
+    stringstream ss;
+
+    for (int j = 1000; j < 1500; j++)
+    {
+      cm.insert(
+        {std::to_string(j), cMapInt(thread_count)}
+      );
+    }
+
+    ss << "I am thread " << this_thread::get_id() << "\n";
+    cout << ss.str() << std::endl;
+    
+  };
+
   vector<future<void>> futures;
-  for (size_t i = 0; i < thread_count; ++i) {
-    futures.push_back(async(std::launch::async, kernel, i));
+  for (size_t i = 0; i < thread_count-2; ++i)
+  {
+    futures.push_back(async(std::launch::async, kernel_inner, i));
+  }
+
+  for (size_t i = thread_count-2; i < thread_count; ++i)
+  {
+    futures.push_back(async(std::launch::async, kernel_outer, i));
   }
 }
 
 void TestAsync()
 {
-  const size_t thread_count = 3;
-  const size_t key_count = 50000;
-  const size_t experiments = 1000;
+  const size_t thread_count = 5;
+  const size_t key_count    = 50000;
+  const size_t experiments  = 1000;
 
   cmap_fold cm;
   for (int i = 0; i < experiments; i++)
@@ -72,6 +95,17 @@ void TestAsync()
 
     for (auto& [k, v] : result) {
       AssertEqual(v, 6, "Key = " + to_string(k));
+    }
+  }
+
+
+  for (int i = experiments; i < experiments+500; i++)
+  {
+    const auto result = std::as_const(cm.at(std::to_string(i))).BuildOrdinaryMap();
+    ASSERT_EQUAL(result.size(), 0);
+
+    for (auto& [k, v] : result) {
+      AssertEqual(v, 0, "Key = " + to_string(k));
     }
   }
 }
